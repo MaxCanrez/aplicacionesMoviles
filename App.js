befc541df7b41ccd95f7545d37fab08ff518a053
MaxCanrez <maxcanrrez@gmail>
2026-03-07 19:48:36 -0600

    Relojes internacionales

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.title}>México</Text>
+      <Text style={styles.clock}>
+        {time.toLocaleTimeString('es-MX', { timeZone: 'America/Mexico_City' })}
+      </Text>
+
+      <Text style={styles.title}>Nueva York</Text>
+      <Text style={styles.clock}>
+        {time.toLocaleTimeString('en-US', { timeZone: 'America/New_York' })}
+      </Text>
+
+      <Text style={styles.title}>Londres</Text>
+      <Text style={styles.clock}>
+        {time.toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })}
+      </Text>
+
+      <Text style={styles.title}>Tokio</Text>
+      <Text style={styles.clock}>
+        {time.toLocaleTimeString('ja-JP', { timeZone: 'Asia/Tokyo' })}
+      </Text>
+
     </View>
   );
 }
@@ -16,5 +45,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 28,
+    marginTop: 15,
+  },
+  clock: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
